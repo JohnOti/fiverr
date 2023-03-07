8f5d5294980d1fc797627ec9ca83cd0cafad275d
thadeus <thadeusnyariki94@gmail.com>
2023-03-07 13:28:13 +0300

    added slide and CardCard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import CatCard from '../../components/catCard/CatCard'
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/slide/Slide'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
+import { cards } from '../../data'
 import './Home.scss'
 
 const Home = () => {
@@ -8,6 +11,11 @@ const Home = () => {
     <div>
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
