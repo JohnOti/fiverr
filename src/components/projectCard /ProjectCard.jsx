c172fecc86414d7fc2ab56f1a135ed6e9f9a2d5b
thadeus <thadeusnyariki94@gmail.com>
2023-03-07 16:11:50 +0300

    Completed fiverr business

---
+++ src/components/projectCard /ProjectCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './ProjectCard.scss'
+
+const ProjectCard = ({item}) => {
+  return (
+    <Link to='/'>
+        <div className='projectCard'>
+            <img src={item.img} alt="" />
+            <span className='desc'>{item.desc}</span>
+            <span className='title'>{item.title}</span>
+        </div>
+    </Link>
+  )
+}
+
+export default ProjectCard
